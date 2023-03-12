f0879114f0d70ca6361d638092248e66606a12b9
Siyu Liu <siyu.java.web@gmail.com>
2023-03-12 14:01:12 +1100

    create comment,post and user routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,4 +1,92 @@
 const router = require('express').Router();
+const { Post, User,Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+            attributes: ["content", "date_created"],
+            include: [
+                {
+                    model: User,
+                    attributes:['username'],
+                }
+            ]
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  console.log(req.session.user_id);
+  console.log(req.body);
+  try {
+    const postData = await Post.update(req.body, {
+      where: {
+            id: req.params.id,
+            user_id: req.session.user_id,
+      },
+    });
+    if (!postData[0]) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+    console.log(postData);
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 module.exports = router;
