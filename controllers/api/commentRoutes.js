d0e53a6c39536da8b408080ab5f6f95dbb8c9a8d
Siyu Liu <siyu.java.web@gmail.com>
2023-03-15 23:54:27 +1100

    add comment delete button and js function

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -47,11 +47,12 @@ router.post("/", withAuth, async (req, res) => {
 });
 
 router.delete("/:id", withAuth, async (req, res) => {
+  console.log(123123);
   try {
     const commentData = await Comment.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        // user_id: req.session.user_id,
       },
     });
 
--- public/js/postDetails.js
+++ public/js/postDetails.js
@@ -1,11 +1,36 @@
-let commentUsers = document.querySelectorAll('.comment-user');
-let updateBtn = document.querySelectorAll('.update-comment-container');
+const commentUsers = document.querySelectorAll('.comment-user');
+const updateBtn = document.querySelectorAll('.update-comment-container');
+const deleteBtn=document.querySelectorAll('.delete-comment-container')
 const user = document.querySelector('.post-container').getAttribute('data-login');
 
+const deleteCommentHandler = async (event) => {
+  event.preventDefault();
+    const id = event.target.getAttribute("data-comment-id");
+    console.log(id);
+  const response = await fetch(`/api/comments/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+    if (response.ok) {
+      post_id=document.querySelector('.post-container').getAttribute('data-post-id')
+    document.location.replace(`/api/posts/details/${post_id}`);
+  } else {
+    alert("Failed to delete comment");
+  }
+};
+
 for (let i = 0; i < commentUsers.length; i++) { 
+    deleteBtn[i].addEventListener("click", deleteCommentHandler);
     if (commentUsers[i].getAttribute('data-user') !== user) { 
         console.log(updateBtn[i]);
         console.log(11111);
         updateBtn[i].style.visibility = "hidden";
+        deleteBtn[i].style.visibility = "hidden";
     }
 }
+
+
+
+
